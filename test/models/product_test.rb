a42b9d5a286b137fea57129da8d9d13c672114da
Tasnim97 <muhdtasnim97@gmail.com>
2018-09-27 06:32:01 +0800

    update

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -5,23 +5,121 @@ class ProductTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "New product No Data" do
-  	product = product.new()
-  	assert_not product.save, "Saved product without a title"
+  def setup
+    @product = Product.new(
+      title: "Paul Reed Smith Paul's Guitar 2013",
+      brand: "PRS",
+      model: "Paul Reed Smith Paul's Guitar",
+      description: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna.",
+      condition: "Excellent",
+      finish: "Red",
+      price: 999.10,
+      quantity: 0,
+      user_id: 3
+    )
   end
 
-  test "New product with Data" do 
-  	  
-  	product = product.new(id: 100,
-  								title: "Paul Reed Smith Paul's Guitar 2013",
-								brand: "PRS",
-								model: "Paul Reed Smith Paul's Guitar",
-								description: "Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna.",
-								condition: "Excellent",
-								finish: "Red",
-								price: "999",
-								image: Rails.root.join("app/assets/images/product1.jpg").open,
-								user_id: 3)
-  	assert_not product.save, "Saved product with a title"
+  test "New product with all correct data" do
+    assert @product.valid?
+  end
+
+  test "New product without title" do 	  
+    @product.title = nil
+    assert @product.valid?, 'saved product without title'
+  end
+
+  test "New product without brand" do    
+    @product.brand = nil
+    assert @product.valid?, 'saved product without brand'
+  end
+
+  test "New product without model" do    
+    @product.model = nil
+    assert @product.valid?, 'saved product without model'
+  end
+
+  test "New product without description" do    
+    @product.description = nil
+    assert @product.valid?, 'saved product without description'
+  end
+
+  test "New product without condition" do    
+    @product.condition = nil
+    assert @product.valid?, 'saved product without condition'
+  end
+
+  test "New product without finish" do    
+    @product.finish = nil
+    assert @product.valid?, 'saved product without finish'
+  end
+
+  test "New product without price" do    
+    @product.price = nil
+    assert @product.valid?, 'saved product without price'
+  end
+
+  test "New product without quantity" do    
+    @product.quantity = nil
+    assert @product.valid?, 'saved product without quantity'
+  end
+
+  test "New product without user_id" do    
+    @product.user_id = nil
+    assert @product.valid?, 'saved product without user_id'
+  end
+
+  test "New product with valid min price" do    
+    @product.price = 0.01
+    assert @product.valid?, 'saved product with min price'
+  end
+
+  test "New product with valid max price" do    
+    @product.price = 99999999.99
+    assert @product.valid?, 'saved product with max price'
+  end
+
+  test "New product with invalid min price" do    
+    @product.price = 0.00
+    assert @product.valid?, 'saved product with invalid min price'
+  end
+
+  test "New product with invalid max price" do    
+    @product.price = 10000000000.00
+    assert @product.valid?, 'saved product with invalid max price'
+  end
+
+  test "New product with invalid format2 price" do    
+    @product.price = "s"
+    assert @product.valid?, 'saved product with invalid price'
+  end
+
+  test "New product with valid min quantity" do    
+    @product.quantity = 0
+    assert @product.valid?, 'saved product with min quantity'
+  end
+
+  test "New product with valid max quantity" do    
+    @product.quantity = 9999999999
+    assert @product.valid?, 'saved product with max quantity'
+  end
+
+  test "New product with invalid min quantity" do    
+    @product.quantity = -1
+    assert @product.valid?, 'saved product with invalid min quantity'
+  end
+
+  test "New product with invalid max quantity" do    
+    @product.quantity = 10000000000
+    assert @product.valid?, 'saved product with invalid max quantity'
+  end
+
+  test "New product with invalid format1 quantity" do    
+    @product.quantity = 0.00
+    assert @product.valid?, 'saved product with invalid quantity 1'
+  end
+
+  test "New product with invalid format2 quantity" do    
+    @product.quantity = "s"
+    assert @product.valid?, 'saved product with invalid quantity 2'
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,40 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(
+      name: "John",
+      email: "John@mail.com",
+      password: "12345678"
+    )
+  end
+
+  test "New user with all correct data" do
+    assert @user.valid?
+  end
+
+  test "New user without name" do 	  
+    @user.name = nil
+    assert @user.valid?, 'user is without a name'
+  end
+
+  test "New user without email" do 	  
+    @user.email = nil
+    assert @user.valid?, 'user is without an email'
+  end
+
+  test "New user without pass" do 	  
+    @user.password = nil
+    assert @user.valid?, 'user is valid without a pass'
+  end
+
+  test "New user with invalid email" do 	  
+    @user.email = "Johnmail.com"
+    assert @user.valid?, 'user is with an invalid email'
+  end
+
+  test "New user with short pass" do 	  
+    @user.password = "1234567"
+    assert @user.valid?, 'user is with a pass that is too short'
+  end
 end
